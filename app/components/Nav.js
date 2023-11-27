a52d01ed80f9043216f2120fa1c2eabe464d738c
Kaden Klein <kadenkleinonline@gmail.com>
2023-11-27 13:26:37 -0700

    Added nav change

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,17 +1,21 @@
-import React from 'react';
+'use client'
+import React, { useState } from 'react';
 import Link from 'next/link'
 
 const Nav = (props) => {
+
+    const [page, setPage] = useState("Construction");
+
     return (
         <nav className="nav">
             <div className="logo">
-                <h1><span className="orange-text">KLEIN</span> Construction</h1>
+                <h1><span className="orange-text">KLEIN</span> {page}</h1>
             </div>
             <div className="nav-links">
                 <div className="navigation">
-                    <Link className="link" href="/design">CONSTRUCTION</Link>
-                    <Link className="link" href="/hvac">HEATING & AIR</Link>
-                    <Link className="link" href="/cabinets">CABINETS</Link>
+                    <Link onClick={() => setPage("Construction")} className="link" href="/design">CONSTRUCTION</Link>
+                    <Link onClick={() => setPage("Heating & Air")} className="link" href="/hvac">HEATING & AIR</Link>
+                    <Link onClick={() => setPage("Cabinets")} className="link" href="/cabinets">CABINETS</Link>
                 </div>
             </div>
         </nav>
